8aa895c3f586fc380c45c029478b207461bc1353
mvpalos <mariepe49@hotmail.com>
2018-07-15 16:24:25 -0400

    AsyncStorage added

--- deploy-frontend/comps/SignIn.js
+++ deploy-frontend/comps/SignIn.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { AppRegistry, StyleSheet, Text, View, TextInput } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, TextInput, TouchableHighlight, AsyncStorage } from 'react-native';
 
 export default class SignIn extends React.Component{
     constructor(){
@@ -8,24 +8,24 @@ export default class SignIn extends React.Component{
         this.state = {
            error: []
         };
-        //this.registerHandler = this.registerHandler.bind(this);
-        //this.removeErrorHandler = this.removeErrorHandler.bind(this);
+        this.registerHandler = this.registerHandler.bind(this);
+        this.removeErrorHandler = this.removeErrorHandler.bind(this);
     }
 
     componentWillMount(){
          //checking validity of JWT (authoritization token)
-//         axios.post('/validtoken',({jwt:localStorage.getItem("jwt")}))
-//         .then((result)=>{
-//             //if there username or password is not sending an error,(account isnt made)
-//             //push the props(of all forms filled) to changing browser endpoint of '/feed'
-//             if(!result.data.error){
-//                 this.props.history.push("/feed");
-//             }
-//         })
-//         //send error if there is existing account
-//         .catch((err)=>{
-//             console.log(err);
-//         })
+         axios.post('/validtoken',({jwt:AsyncStorage.getItem("jwt")}))
+         .then((result)=>{
+             //if there username or password is not sending an error,(account isnt made)
+             //push the props(of all forms filled) to changing browser endpoint of '/feed'
+             if(!result.data.error){
+                 this.props.history.push("/home");
+             }
+         })
+         //send error if there is existing account
+         .catch((err)=>{
+             console.log(err);
+         })
   }
 
 registerHandler(e){
@@ -70,10 +70,14 @@ removeErrorHandler(){
     render(){
         return(
             <View>
-                <Text style = {styles.title}>Hello.</Text>
-                <TextInput style = {styles.entryText} placeholder = "Username" />
-                <TextInput style = {styles.entryText} placeholder = "Password" />
+                <Text style = {styles.title}>Login</Text>
+                <TextInput style = {styles.entryText} placeholder = "Username"/>
+                <TextInput style = {styles.entryText} placeholder = "Password"/>
 
+                <TouchableHighlight stlye = {styles.buttonText} onPress >
+                <Text style = {styles.button}>Sign In</Text>
+
+                </TouchableHighlight>
             </View>
         )
     }
@@ -86,7 +90,25 @@ const styles = StyleSheet.create({
     },
     entryText :{
         fontSize: 20,
-        padding: 30,
+        padding: 15,
+        color: 'black',
+        borderWidth: .45,
+        borderColor: '#48BBEC',
+        borderRadius: 1
+    },
+    buttonText: {
+        fontSize: 18,
+        color: 'black',
+        alignSelf: 'center'
+    },
+    button: {
+        height: 40,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        marginLeft: 10,
+        width: 80,
+        alignSelf: 'stretch',
+        justifyContent: 'center'
     }
 
 });
